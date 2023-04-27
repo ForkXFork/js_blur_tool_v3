31d821bd6a390e323d4e429bfa8a3ec1cd98f558
mgar216 <28987804+mgar216@users.noreply.github.com>
2023-04-27 14:32:55 -0600

    Update script.js
    Fixed errors.

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-let image = new Image();
-let isMouseDown = false;
-let startX, startY, endX, endY;
-
 document.addEventListener("DOMContentLoaded", () => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
     let img = new Image();
     let pixelatedImgData;
+    let isSelecting = false;
+    let selectionStart;
+    let selectionEnd;
   
     document.getElementById("imageFile").addEventListener("change", (e) => {
       const file = e.target.files[0];
@@ -28,105 +25,89 @@ document.addEventListener("DOMContentLoaded", () => {
       };
       reader.readAsDataURL(file);
     });
-
-    function loadImage(file) {
-        const reader = new FileReader();
-        reader.onload = function (e) {
-            image.src = e.target.result;
-            image.onload = function () {
-                ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-                offscreenCtx.drawImage(image, 0, 0, offscreenCanvas.width, offscreenCanvas.height);
-            };
-        };
-        reader.readAsDataURL(file);
+  
+    function pixelate(start, end) {
+      const pixelSize = 10;
+      const x = Math.min(start.x, end.x);
+      const y = Math.min(start.y, end.y);
+      const w = Math.abs(end.x - start.x);
+      const h = Math.abs(end.y - start.y);
+  
+      ctx.putImageData(pixelatedImgData, 0, 0);
+      for (let i = x; i < x + w; i += pixelSize) {
+        for (let j = y; j < y + h; j += pixelSize) {
+          const color = getAverageColor(i, j, pixelSize, pixelSize);
+          ctx.fillStyle = `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`;
+          ctx.fillRect(i, j, pixelSize, pixelSize);
+        }
+      }
+  
+      pixelatedImgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     }
-
-
-    function drawSelectionRect() {
-        ctx.drawImage(offscreenCanvas, 0, 0);
-        ctx.strokeStyle = 'rgba(0, 0, 255, 0.5)';
+  
+    function getAverageColor(x, y, w, h) {
+      const data = pixelatedImgData.data;
+      let color = { r: 0, g: 0, b: 0, a: 0 };
+      let count = 0;
+  
+      for (let i = x; i < x + w; i++) {
+        for (let j = y; j < y + h; j++) {
+          const index = (i + j * canvas.width) * 4;
+          color.r += data[index];
+          color.g += data[index + 1];
+          color.b += data[index + 2];
+          color.a += data[index + 3];
+          count++;
+        }
+      }
+      color.r = Math.round(color.r / count);
+      color.g = Math.round(color.g / count);
+      color.b = Math.round(color.b / count);
+      color.a = Math.round(color.a / count);
+  
+      return color;
+    }
+  
+    canvas.addEventListener("mousedown", (e) => {
+      isSelecting = true;
+      selectionStart = getMousePos(canvas, e);
+    });
+  
+    canvas.addEventListener("mousemove", (e) => {
+      if (isSelecting) {
+        selectionEnd = getMousePos(canvas, e);
+        ctx.putImageData(pixelatedImgData, 0, 0);
+        ctx.strokeStyle = "#00bcd4";
         ctx.lineWidth = 2;
-        ctx.strokeRect(startX, startY, endX - startX, endY - startY);
-    }
-
-    function applyPixelation() {
-        const width = endX - startX;
-        const height = endY - startY;
-        const pixelSize = 10;
-
-        for (let y = 0; y < height; y += pixelSize) {
-            for (let x = 0; x < width; x += pixelSize) {
-                const pixelData = offscreenCtx.getImageData(startX + x, startY + y, pixelSize, pixelSize);
-                const avgColor = getAverageColor(pixelData.data);
-
-                offscreenCtx.fillStyle = `rgb(${avgColor.r}, ${avgColor.g}, ${avgColor.b})`;
-                offscreenCtx.fillRect(startX + x, startY + y, pixelSize, pixelSize);
-            }
+        ctx.setLineDash([6]);
+        const rectWidth = Math.abs(selectionEnd.x - selectionStart.x);
+        const rectHeight = Math.abs(selectionEnd.y - selectionStart.y);
+        const startX = Math.min(selectionStart.x, selectionEnd.x);
+        const startY = Math.min(selectionStart.y, selectionEnd.y);
+        ctx.strokeRect(startX, startY, rectWidth, rectHeight);
+      }
+    });
+    canvas.addEventListener("mouseup", (e) => {
+        if (isSelecting) {
+          isSelecting = false;
+          selectionEnd = getMousePos(canvas, e);
+          pixelate(selectionStart, selectionEnd);
+          pixelatedImgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
         }
-
-        ctx.drawImage(offscreenCanvas, 0, 0);
-    }
-
-    function getAverageColor(data) {
-        let r = 0, g = 0, b = 0, count = 0;
-
-        for (let i = 0; i < data.length; i += 4) {
-            r += data[i];
-            g += data[i + 1];
-            b += data[i + 2];
-            count++;
-        }
-
-        return { r: r / count, g: g / count, b: b / count };
-    }
-
-    const offscreenCanvas = document.createElement('canvas');
-    offscreenCanvas.width = canvas.width;
-    offscreenCanvas.height = canvas.height;
-    const offscreenCtx = offscreenCanvas.getContext('2d');
-
-
-    canvas.addEventListener('mousedown', (e) => {
-        isMouseDown = true;
-        startX = e.clientX - canvas.offsetLeft;
-        startY = e.clientY - canvas.offsetTop;
+      });
+    
+      function getMousePos(canvas, evt) {
+        const rect = canvas.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top,
+        };
+      }
+    
+      document.getElementById("saveButton").addEventListener("click", () => {
+        const link = document.createElement("a");
+        link.download = "pixelated-image.png";
+        link.href = canvas.toDataURL("image/png");
+        link.click();
+      });
     });
-
-    canvas.addEventListener('mousemove', (e) => {
-        if (!isMouseDown) return;
-        endX = e.clientX - canvas.offsetLeft;
-        endY = e.clientY - canvas.offsetTop;
-        drawSelectionRect();
-    });
-
-    canvas.addEventListener('mouseup', (e) => {
-        if (!isMouseDown) return;
-        isMouseDown = false;
-        endX = e.clientX - canvas.offsetLeft;
-        endY = e.clientY - canvas.offsetTop;
-        drawSelectionRect();
-        applyPixelation();
-    });
-
-    document.getElementById('imageFile').addEventListener('change', (e) => {
-        loadImage(e.target.files[0]);
-    });
-
-    document.getElementById('saveButton').addEventListener('click', () => {
-        const filename = prompt('Enter the filename:', 'pixelated_image.png');
-        if (filename === null || filename.trim() === '') {
-            // User pressed cancel or entered an empty filename, so do nothing
-            return;
-        }
-
-        canvas.toBlob((blob) => {
-            const link = document.createElement('a');
-            link.href = URL.createObjectURL(blob);
-            link.download = filename;
-            link.style.display = 'none';
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        }, 'image/png');
-    });
-});
